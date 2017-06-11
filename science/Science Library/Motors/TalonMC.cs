d91eace06e03399bfad8510d084ba7e1378862b2
Jaden Bottemiller <jadenbottemiller@gmail.com>
2017-06-11 15:41:48 -0700

    Added motor and encoder files

---
+++ science/Science Library/Motors/TalonMC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboticsLibrary.Motors
+{
+    class TalonMC : Motor
+    {
+
+        public TalonMC()
+        {
+
+        }
+
+    }
+}
