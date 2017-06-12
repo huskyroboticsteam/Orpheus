26dd20846494d0fe4db520a79ac03682d44f19e8
Cai Biesinger <Cai.Biesinger@outlook.com>
2017-06-12 00:05:22 -0700

    Appending motor structure.
    Forgot to include these two files in the last commit. Oops.

---
+++ science/Science Library/Motors/IMotor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RoboticsLibrary.Motors
+{
+    public interface IMotor
+    {
+        // Sets speed, and checks parameters such as current and timeouts for issues.
+        void UpdateState();
+
+        // Re-homes the device attached to this motor. This can take a while.
+        void Initialize();
+
+        // Stops the motor.
+        void Stop();
+
+        // Processes an event trigger from something like a limit switch.
+        void EventTriggered(object Sender, EventArgs Event);
+    }
+}
--- science/Science Library/Motors/Motor.cs
+++ science/Science Library/Motors/Motor.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using RoboticsLibrary.Sensors;
 
 namespace RoboticsLibrary.Motors
 {
-    class Motor
+    
+    public abstract class Motor : IMotor
     {
+        /// <summary>
+        /// A generic DC motor. This class is usually subclassed for individual motors.
+        /// </summary>
+        /// <param name="Pin">The pin on which to output from to drive this motor.</param>
+        public Motor(int Pin)
+        {
+
+        }
+
+        public abstract void EventTriggered(object Sender, EventArgs Event);
+
+        public abstract void Initialize();
+
+        public abstract void Stop();
+
+        public abstract void UpdateState();
     }
 }
