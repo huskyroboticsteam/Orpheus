dc5797c3880f5c558621855ffa508ba854fdd881
Cai Biesinger <Cai.Biesinger@outlook.com>
2017-06-11 17:01:39 -0700

    Began working on error codes.
    Ready for some error codes to actually be entered.

--- science/Science Library/Errors/ErrorCodes.cs
+++ science/Science Library/Errors/ErrorCodes.cs
@@ -4,9 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Science_Library.Errors
+namespace RoboticsLibrary.Errors
 {
     public static class ErrorCodes
     {
+        public static readonly string[][] List = new string[][] { new string[]
+        { // Main System
+            "Everything is OK",
+            "Unknown error"
+        }, new string[]
+        { // Thermocouple
+            "",
+            ""
+        }, new string[]
+        { // UV Sensor
+            "",
+            ""
+        }, new string[]
+        { // Distance Sensor
+            "",
+            ""
+        }, new string[]
+        { // Humidity Sensor
+            "",
+            ""
+        }, new string[]
+        { // Communications
+            "",
+            ""
+        }, new string[]
+        { // Motors
+            "",
+            ""
+        }};
     }
 }
