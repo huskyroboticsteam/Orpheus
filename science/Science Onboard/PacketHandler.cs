f043ab4f5bea69105e9bea72869d38dac85e68cf
Jaden Bottemiller <jadenbottemiller@gmail.com>
2017-06-13 14:07:03 -0700

    Added sample packet parsing architecture

---
+++ science/Science Onboard/PacketHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoboticsLibrary.Communications;
+using RoboticsLibrary.Utilities;
+
+namespace Science
+{
+    public class PacketHandler
+    {
+
+        public PacketHandler()
+        {
+            Parse.SetParseHandler(0x01, ParseErrorPacket);
+        }
+
+        public static void ParseErrorPacket(Message Error)
+        {
+            Log.Output(Log.Severity.DEBUG, Log.Source.NETWORK, "Error Packet Received.");
+        }
+
+    }
+}
