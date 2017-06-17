eb5cb2b105ee75f52a08e9482fab8aee2826b5ff
Cai Biesinger <Cai.Biesinger@outlook.com>
2017-06-17 15:01:03 -0700

    About to make massive library changes.

--- science/Science Library/Science/PacketType.cs
+++ science/Science Library/Science/PacketType.cs
@@ -8,8 +8,18 @@ namespace RoboticsLibrary.Science
 {
     public static class PacketType
     {
+        #region Bidirectional (0x00 through 0x7F)
         public const byte WATCHDOG_PING = 0x00;
         public const byte ERROR = 0x01;
-        public const byte EMERGENCY_STOP = 0x02;
+        #endregion
+
+        #region Base to Rover (0x80 through 0xBF)
+        public const byte EMERGENCY_STOP = 0x80;
+        public const byte IMAGE_REQUEST = 0x81;
+        #endregion
+
+        #region Rover to Base (0xC0 through 0xFF)
+        public const byte SENSOR_READINGS = 0xC0;
+        #endregion
     }
 }
