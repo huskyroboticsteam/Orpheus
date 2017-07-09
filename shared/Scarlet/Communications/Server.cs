94cb3526fd21133e62e49016ab0e295115858615
Cai Biesinger <Cai.Biesinger@outlook.com>
2017-07-09 13:47:45 -0700

    New Server
    Began work on new server implementation.

---
+++ shared/Scarlet/Communications/Server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scarlet.Communications
+{
+    public static class Server
+    {
+
+
+        public static void Start(int PortTCP, int PortUDP, int ReceiveBufferSize = 64, int OperationPeriod = 20)
+        {
+
+        }
+
+        public static void Stop()
+        {
+
+        }
+    }
+}
