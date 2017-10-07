93c2c5b837f6acc2567d4c3c098598ffcb820206
Baldstrom <jadenbottemiller@gmail.com>
2017-10-07 14:04:12 -0700

    Added UART to Pi- need to compile .so UART wiringPi library and test.

---
+++ Shared/Scarlet/IO/RaspberryPi/UARTBusPi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scarlet.IO.RaspberryPi
+{
+    public class UARTBusPi : IUARTBus
+    {
+        private int DeviceID;
+
+        public UARTBusPi(byte Device, int Baud)
+        {
+            DeviceID = RaspberryPi.SerialOpen(Device, Baud);
+        }
+
+        public void Initialize() { }
+
+        public byte[] Read(int Length)
+        {
+            List<byte> Data = new List<byte>();
+            int Avail = RaspberryPi.SerialDataAvailable(DeviceID);
+            if (Avail < Length) { Length = Avail; }
+            for (int i = 0; i < Length; i++)
+            {
+                Data.Add(RaspberryPi.SerialGetChar(DeviceID));
+            }
+            return Data.ToArray();
+        }
+
+        public void Write(byte[] Data) { RaspberryPi.SerialPut(DeviceID, Data); }
+
+        public void Dispose() { throw new NotImplementedException(); }
+
+    }
+}
