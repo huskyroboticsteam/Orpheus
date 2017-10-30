a48db94f13dbc8b8be04f7f98c0071eafe109918
Jaden Bottemiller <jadenbottemiller@gmail.com>
2017-10-30 14:28:54 -0700

    Added and tested TryToString() to UtilData

--- Shared/Scarlet/Utilities/UtilData.cs
+++ Shared/Scarlet/Utilities/UtilData.cs
@@ -6,22 +6,22 @@ namespace Scarlet.Utilities
 {
     public static class UtilData
     {
-        internal static byte[] Reverse(byte[] Input)
+        internal static byte[] EnsureBigEndian(byte[] Input)
         {
             if (BitConverter.IsLittleEndian) { Array.Reverse(Input); }
             return Input;
         }
 
-        public static byte[] ToBytes(bool Input) { return Reverse(BitConverter.GetBytes(Input)); }
-        public static byte[] ToBytes(char Input) { return Reverse(BitConverter.GetBytes(Input)); }
-        public static byte[] ToBytes(double Input) { return Reverse(BitConverter.GetBytes(Input)); }
-        public static byte[] ToBytes(float Input) { return Reverse(BitConverter.GetBytes(Input)); }
-        public static byte[] ToBytes(int Input) { return Reverse(BitConverter.GetBytes(Input)); }
-        public static byte[] ToBytes(long Input) { return Reverse(BitConverter.GetBytes(Input)); }
-        public static byte[] ToBytes(short Input) { return Reverse(BitConverter.GetBytes(Input)); }
-        public static byte[] ToBytes(uint Input) { return Reverse(BitConverter.GetBytes(Input)); }
-        public static byte[] ToBytes(ulong Input) { return Reverse(BitConverter.GetBytes(Input)); }
-        public static byte[] ToBytes(ushort Input) { return Reverse(BitConverter.GetBytes(Input)); }
+        public static byte[] ToBytes(bool Input) { return EnsureBigEndian(BitConverter.GetBytes(Input)); }
+        public static byte[] ToBytes(char Input) { return EnsureBigEndian(BitConverter.GetBytes(Input)); }
+        public static byte[] ToBytes(double Input) { return EnsureBigEndian(BitConverter.GetBytes(Input)); }
+        public static byte[] ToBytes(float Input) { return EnsureBigEndian(BitConverter.GetBytes(Input)); }
+        public static byte[] ToBytes(int Input) { return EnsureBigEndian(BitConverter.GetBytes(Input)); }
+        public static byte[] ToBytes(long Input) { return EnsureBigEndian(BitConverter.GetBytes(Input)); }
+        public static byte[] ToBytes(short Input) { return EnsureBigEndian(BitConverter.GetBytes(Input)); }
+        public static byte[] ToBytes(uint Input) { return EnsureBigEndian(BitConverter.GetBytes(Input)); }
+        public static byte[] ToBytes(ulong Input) { return EnsureBigEndian(BitConverter.GetBytes(Input)); }
+        public static byte[] ToBytes(ushort Input) { return EnsureBigEndian(BitConverter.GetBytes(Input)); }
         public static byte[] ToBytes(string Input)
         {
             if(Input == null || Input.Length == 0) { return new byte[0]; }
@@ -38,63 +38,70 @@ namespace Scarlet.Utilities
         public static bool ToBool(byte[] Input)
         {
             if (Input.Length != 1) { throw new FormatException("Given byte[] does not convert to bool."); }
-            return BitConverter.ToBoolean(Reverse(Input), 0);
+            return BitConverter.ToBoolean(EnsureBigEndian(Input), 0);
         }
 
         public static char ToChar(byte[] Input)
         {
             if (Input.Length != 2) { throw new FormatException("Given byte[] does not convert to char."); }
-            return BitConverter.ToChar(Reverse(Input), 0);
+            return BitConverter.ToChar(EnsureBigEndian(Input), 0);
         }
 
         public static double ToDouble(byte[] Input)
         {
             if (Input.Length != 8) { throw new FormatException("Given byte[] does not convert to double."); }
-            return BitConverter.ToDouble(Reverse(Input), 0);
+            return BitConverter.ToDouble(EnsureBigEndian(Input), 0);
         }
 
         public static float ToFloat(byte[] Input)
         {
             if (Input.Length != 4) { throw new FormatException("Given byte[] does not convert to float."); }
-            return BitConverter.ToSingle(Reverse(Input), 0);
+            return BitConverter.ToSingle(EnsureBigEndian(Input), 0);
         }
 
         public static int ToInt(byte[] Input)
         {
             if (Input.Length != 4) { throw new FormatException("Given byte[] does not convert to int."); }
-            return BitConverter.ToInt32(Reverse(Input), 0);
+            return BitConverter.ToInt32(EnsureBigEndian(Input), 0);
         }
 
         public static long ToLong(byte[] Input)
         {
             if (Input.Length != 8) { throw new FormatException("Given byte[] does not convert to long."); }
-            return BitConverter.ToInt64(Reverse(Input), 0);
+            return BitConverter.ToInt64(EnsureBigEndian(Input), 0);
         }
 
         public static short ToShort(byte[] Input)
         {
             if (Input.Length != 2) { throw new FormatException("Given byte[] does not convert to short."); }
-            return BitConverter.ToInt16(Reverse(Input), 0);
+            return BitConverter.ToInt16(EnsureBigEndian(Input), 0);
         }
 
         public static uint ToUInt(byte[] Input)
         {
             if (Input.Length != 4) { throw new FormatException("Given byte[] does not convert to uint."); }
-            return BitConverter.ToUInt32(Reverse(Input), 0);
+            return BitConverter.ToUInt32(EnsureBigEndian(Input), 0);
         }
 
         public static ulong ToULong(byte[] Input)
         {
             if (Input.Length != 8) { throw new FormatException("Given byte[] does not convert to ulong."); }
-            return BitConverter.ToUInt64(Reverse(Input), 0);
+            return BitConverter.ToUInt64(EnsureBigEndian(Input), 0);
         }
 
         public static ushort ToUShort(byte[] Input)
         {
             if (Input.Length != 2) { throw new FormatException("Given byte[] does not convert to ushort."); }
-            return BitConverter.ToUInt16(Reverse(Input), 0);
+            return BitConverter.ToUInt16(EnsureBigEndian(Input), 0);
         }
 
+        /// <summary>
+        /// Returns a string from the byte
+        /// representation of the string in
+        /// unicode
+        /// </summary>
+        /// <param name="Input">Byte represensation of the unicode string</param>
+        /// <returns>String representation of the bytes</returns>
         public static string ToString(byte[] Input)
         {
             if (Input == null || Input.Length == 0 || Input.Length % 2 == 1) { throw new FormatException("Given byte[] does not convert to string."); }
@@ -104,6 +111,20 @@ namespace Scarlet.Utilities
                 Output.Append((char)(Input[i] << 8 | Input[i + 1]));
             }
             return Output.ToString();
+        }
+
+        /// <summary>
+        /// Tries to convert a byte input (unicode) to a string
+        /// </summary>
+        /// <param name="Input">Bytes to convert to string</param>
+        /// <param name="Output">Output of the conversion. (Null if failed)</param>
+        /// <returns>True if string conversion succeeds</returns>
+        public static bool TryToString(byte[] Input, out string Output)
+        {
+            Output = null; 
+            try { Output = ToString(Input); }
+            catch { return false; }
+            return true;
         }
 
         /// <summary>
