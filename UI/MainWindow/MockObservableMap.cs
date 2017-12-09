871974258fd60dda76bf16b8fc78b8fcf3d1fed8
Ethan Shea <ethan.shea1@gmail.com>
2017-12-09 11:32:50 -0800

    Add ObservableMap

---
+++ UI/MainWindow/MockObservableMap.cs
@@ -0,0 +1,45 @@
+﻿using HuskyRobotics.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HuskyRobotics.UI
+{
+    public class MockObservableMap : ObservableMap<float>
+    {
+        private IList<string> fields = new List<string> {
+            "chasis/wheel1/steeringAngle",
+            "chasis/wheel2/steeringAngle",
+            "chasis/wheel3/steeringAngle",
+            "chasis/wheel4/steeringAngle",
+            "test",
+        };
+        private Timer updater;
+
+        public MockObservableMap()
+        {
+            updateValues();
+            updater = new Timer(updateValues, null, 400, 400);
+        }
+        
+        private void updateValues()
+        {
+            updateValues(null);
+        }
+
+        private void updateValues(object state)
+        {
+            var rad = new Random();
+            foreach (string field in fields) {
+                var currentValue = ContainsKey(field) ? GetValue(field) : rad.NextDouble();
+                var newValue = Math.Min(Math.Max(currentValue + (rad.NextDouble() - .5) * .05, 0), 1);
+                PutValue(field, (float) newValue);
+            }
+        }
+
+
+    }
+}
---
+++ UI/Utilities/ObservableMap.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuskyRobotics.Utilities
+{
+    public class ObservableMap<T> : INotifyPropertyChanged
+    {
+        private IDictionary<String, MeasuredValue<T>> dictionary = new Dictionary<String, MeasuredValue<T>>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Gets the string representation of the value at the given dictionary key. Designed for bindings
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public MeasuredValue<T> this[string key] {
+            get => dictionary[key];
+        }
+
+        public bool ContainsKey(string key) => dictionary.ContainsKey(key);
+
+        public T GetValue(string key) => dictionary[key].Value;
+
+        public void PutValue(string key, T value)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary[key].Value = value;
+            } else
+            {
+                dictionary[key] = new MeasuredValue<T>(value);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Count"));
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Item[" + key + "]"));
+        }
+
+        public int Count {
+            get => dictionary.Count;
+        }
+    }
+
+    public class MeasuredValue<T> : INotifyPropertyChanged
+    {
+        public bool IsPresent {
+            get => _value != null;
+        }
+
+        public MeasuredValue(T value)
+        {
+            _value = value;
+            _lastUpdated = DateTime.Now;
+        }
+
+        private T _value;
+        public T Value {
+            get => _value;
+            set {
+                _value = value;
+                LastUpdated = DateTime.Now;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Value"));
+            }
+        }
+
+        public DateTime _lastUpdated;
+        public DateTime LastUpdated {
+            get => _lastUpdated;
+            private set {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LastUpdated"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
