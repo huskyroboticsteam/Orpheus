89b25db3d6f4d8c665e8b86acef1aca09ca1ea94
Jeremy Lipschutz <jeremyl@D-108-179-150-190.dhcp4.washington.edu>
2018-01-20 10:31:37 -0800

    add enum of DriverTypes

--- Rover/FlatRobot/FlatRobot/Program.cs
+++ Rover/FlatRobot/FlatRobot/Program.cs
@@ -21,11 +21,47 @@ namespace FlatRobot
         {
             return State.Triggers.Left <= Double.Epsilon && State.Triggers.Right <= Double.Epsilon;
         }
-
+        enum DriveTypes
+        {
+            GTA,
+            Tank,
+            OnePunch,
+        }
         public static void Main(string[] args)
         {
-            if (!float.TryParse(args[1], out MAX_SPEED))
-                MAX_SPEED = 0.2f;
+            DriveTypes drive_type = DriveTypes.OnePunch;
+            for (int i = 0; i < args.Length; i += 2)
+            {
+                switch(args[i])
+                {
+                    case "-speed":
+                        if(!float.TryParse(args[i + 1], out MAX_SPEED))
+                        {
+                            Console.WriteLine("Unable to parse speed. Defaulting to 0.2");
+                            MAX_SPEED = 0.2f;
+                        }
+                        break;
+                    case "-drive":
+                        switch(args[i + 1].ToLower())
+                        {
+                            case "gta":
+                                drive_type = DriveTypes.GTA;
+                                break;
+                            case "tank":
+                                drive_type = DriveTypes.Tank;
+                                break;
+                            case "onepunch":
+                            default:
+                                drive_type = DriveTypes.OnePunch;
+                                break;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Unrecognized parameter");
+                        break;
+
+                }
+            }
             
             Console.WriteLine("Initializing");
             StateStore.Start("Evan and Jeremy");
@@ -49,7 +85,8 @@ namespace FlatRobot
             // IPWMOutput OutC = PWMBBB.PWMDevice1.OutputA;
             // IPWMOutput OutD = PWMBBB.PWMDevice1.OutputA;
 
-            //Sets the Direction Outputs in the cytron to false (don't know what they do)
+            //Sets the Direction Outputs in the cytron to false (don't know what they do) (Figured it out)
+            // This SetOutput when false or true affects what direction is forward and backwards
             Motor1Output.SetOutput(false);
             Motor2Output.SetOutput(false);
 
@@ -85,25 +122,25 @@ namespace FlatRobot
                     Console.WriteLine("NOT CONNECTED");
                 }
 
-                switch (args[0].ToLower())
+                switch (drive_type)
                 {
-                    case "gta":                
+                    case DriveTypes.GTA:                
                         //GTA Drive
                         float GTASpeed = State.Triggers.Right - State.Triggers.Left;
                         float thumbstickX = State.ThumbSticks.Left.X;
                         driver.Move(thumbstickX * MAX_SPEED, GTASpeed * MAX_SPEED);
                         break;                
-                    case "tank":
+                    case DriveTypes.Tank:
                         // Tank Drive                        
                         Motor[0].SetSpeed((float)((State.ThumbSticks.Right.Y) * 1.0f));
                         Motor[1].SetSpeed((float)((State.ThumbSticks.Left.Y) * 1.0f));
                         break;                        
-                    case "onepunch":
+                    case DriveTypes.OnePunch:
                     default:
                         // One-Thumbstick Drive
                         driver.Move((float)(State.ThumbSticks.Left.X) * MAX_SPEED, (float)(State.ThumbSticks.Left.Y) * MAX_SPEED);
                         break;
-            }
+                }   
                    
             } while (State.Buttons.Start != ButtonState.Pressed);
 
