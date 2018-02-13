e283d368956da5bc85bb16f00fda906f7bb6cd37
Emela Moreyra <emelavm@gmail.com>
2018-02-12 17:52:50 -0800

    Made changes requested in the pull request as well as cleaned up a bit more and updated comments

--- BaseStation/VideoStreamer/MainWindow.xaml.cs
+++ BaseStation/VideoStreamer/MainWindow.xaml.cs
@@ -24,8 +24,6 @@ using System.Windows.Threading;
 // this program is a video player that uses G-streamer through the console with file saving
 // capabilities.
 // Gstreamer must be installed to the C drive
-// TO DO:
-// find a final loaction for ports file and create generic destination to work on any computer
 
 // server command:
 // IP will cange when switching to basestation computer
@@ -37,19 +35,20 @@ namespace VideoStreamer
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private DispatcherTimer recordingTime;
+        private DateTime recordingStartTime;
         private DateTime duration;
         private String portFileLocation;
-        private String _destination;
-        public String Destination
+        private String _recordingFolder;
+        public String RecordingFolder
         {
-            get { return _destination; }
+            get { return _recordingFolder; }
             set
             {
-                _destination = value;
+                _recordingFolder = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Destinaion"));
             }
         }
-        private Boolean playing;
+        private bool playing;
 
         private ObservableCollection<int> ports;
         private Process[] players;
@@ -69,8 +68,7 @@ namespace VideoStreamer
             recordingTime.Interval = new TimeSpan(0, 0, 1);
             duration = new DateTime(0);
             DataContext = this;
-            Destination = @"C:\Users\emela\Desktop\";
-            // I know this is bad but I don't know where it should go
+            RecordingFolder = Directory.GetCurrentDirectory();
             portFileLocation = Directory.GetCurrentDirectory() + @"\ports.txt";
 
             ports = new ObservableCollection<int>();
@@ -95,100 +93,106 @@ namespace VideoStreamer
             file.Close();
         }
 
-        // initializes the processes used to launch the stream and file recorder
-        private void initProcesses(ref Process[] pro)
-        {
-            pro = new Process[ports.Count];
-            for (int i = 0; i < ports.Count; i++)
-            {
-                pro[i] = new Process();
-                pro[i].StartInfo.RedirectStandardInput = false;
-                pro[i].StartInfo.UseShellExecute = true;
-            }
-        }
-
         // handles the time increase for the timer
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            duration = duration.AddSeconds(1);
+            duration = new DateTime(DateTime.Now.Subtract(recordingStartTime).Ticks);
             time.Content = "Recording Duration: " + duration.ToString("HH:mm:ss");
         }
 
         // initiates the recording of the stream
         private void ButtonRecord(object sender, RoutedEventArgs e)
         {
-            if (!playing)
+            if (File.Exists(@"C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe"))
             {
-                ButtonLaunch(null, null);
+                if (!playing)
+                {
+                    ButtonLaunch(null, null);
+                }
+                recorders = new Process[ports.Count];
+                recordingStartTime = DateTime.Now;
+                for (int i = 0; i < ports.Count; i++)
+                {
+                    makeRecorderShellProcess(recorders[i], ports.ElementAt(i), i);
+                }
             }
-            initProcesses(ref recorders);
-            for (int i = 0; i < ports.Count; i++)
+            else
             {
-                recordStream(ports.ElementAt(i), recorders[i], i);
+                System.Windows.MessageBox.Show(@"Please ensure that gst-launch-1.0.exe is located in C:\gstreamer\1.0\x86_64\bin\");
             }
             duration = new DateTime(0);
             time.Content = "Recording Duration: " + duration.ToString("HH:mm:ss");
             recordingTime.Start();
         }
 
-        // plays the video streams in their own windows
+        // plays the video streams in their own windows and if already playing will close them
         private void ButtonLaunch(object sender, RoutedEventArgs e)
         {
             if (playing)
             {
                 playing = false;
                 btnLaunch.Content = "Launch";
-                closeAll(players);
+                closeAllPlayers();
             }
             else
             {
-                playing = true;
-                btnLaunch.Content = "Stahp";
-                initProcesses(ref players);
-                for (int i = 0; i < ports.Count; i++)
+                if (File.Exists(@"C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe"))
                 {
-                    startStream(ports.ElementAt(i), players[i]);
+                    playing = true;
+                    btnLaunch.Content = "Stahp";
+                    players = new Process[ports.Count];
+                    for (int i = 0; i < ports.Count; i++)
+                    {
+                        startStreamPlayer(ports.ElementAt(i), ref players[i]);
+                    }
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show(@"Please ensure that gst-launch-1.0.exe is located in C:\gstreamer\1.0\x86_64\bin\");
                 }
             }
         }
 
-        // closes all windows in the array of processes given
-        private void closeAll(Process[] cmd)
+        // closes all video player windows
+        private void closeAllPlayers()
         {
-            for (int i = 0; i < cmd.Length; i++)
+            for (int i = 0; i < players.Length; i++)
             {
-                cmd[i].CloseMainWindow();
+                players[i].CloseMainWindow();
             }
         }
 
-        // initiates a stream with a given port
-        private void startStream(int port, Process cmd)
+        // initiates a player stream with a given port
+        private void startStreamPlayer(int port, ref Process pro)
         {
-            cmd.StartInfo.FileName = "cmd.exe";
-            cmd.StartInfo.RedirectStandardInput = true;
-            cmd.StartInfo.UseShellExecute = false;
-            cmd.Start();
-            cmd.StandardInput.WriteLine(@"set PATH=%PATH%;C:\gstreamer\1.0\x86_64\bin");
-            cmd.StandardInput.WriteLine("gst-launch-1.0 udpsrc caps=\"application/x-rtp," +
+            pro = new Process();
+            pro.StartInfo.FileName = "cmd.exe";
+            pro.StartInfo.RedirectStandardInput = true;
+            pro.StartInfo.UseShellExecute = false;
+            pro.Start();
+            pro.StandardInput.WriteLine(@"set PATH=%PATH%;C:\gstreamer\1.0\x86_64\bin");
+            pro.StandardInput.WriteLine("gst-launch-1.0 udpsrc caps=\"application/x-rtp," +
                 " media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264\" port=" +
                 port + " ! rtph264depay ! decodebin ! videoconvert ! autovideosink");
         }
 
-        // saves the stream to a file from the given port, destination is set from the control panel
-        // and a pseudo-random name is generated for the file using the current date and time
-        private void recordStream(int port, Process cmd, int cam)
+        // initializes and launches the file recording processes
+        private void makeRecorderShellProcess(Process pro, int port, int cam)
         {
-            cmd.StartInfo.WorkingDirectory = Destination;
-            cmd.StartInfo.FileName = @"C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe";
-            cmd.StartInfo.Arguments = "-m -e udpsrc caps=\"application/x-rtp," +
+            pro = new Process();
+            pro.StartInfo.RedirectStandardInput = false;
+            pro.StartInfo.UseShellExecute = true;
+            pro.StartInfo.WorkingDirectory = RecordingFolder;
+            pro.StartInfo.FileName = @"C:\gstreamer\1.0\x86_64\bin\gst-launch-1.0.exe";
+            pro.StartInfo.Arguments = "-m -e udpsrc caps=\"application/x-rtp," +
                 " media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264\" port=" +
                 port + " ! rtph264depay ! h264parse ! mp4mux ! filesink location=" +
                 DateTime.Now.ToString("MM-dd-yyyy--HH;mm;ss") + "(" + cam + ")" + ".mp4";
-            cmd.Start();
+            pro.Start();
         }
 
         // adds ports to the port list
-        private void Add(object sender, RoutedEventArgs e)
+        private void AddPort_Textbox(object sender, RoutedEventArgs e)
         {
             int x;
             if (Int32.TryParse(portBox.Text, out x))
@@ -199,7 +203,7 @@ namespace VideoStreamer
         }
 
         // removes selected items from the port list
-        private void Remove(object sender, RoutedEventArgs e)
+        private void RemoveSelectedPorts(object sender, RoutedEventArgs e)
         {
             List<int> remove = new List<int>();
             foreach (int thing in portList.SelectedItems)
@@ -220,7 +224,7 @@ namespace VideoStreamer
             System.Windows.MessageBox.Show("Please close all recording streams using Ctrl+C");
             try
             {
-                closeAll(players);
+                closeAllPlayers();
             }
             catch { }
 
