fd1918ec44c85d9bde7be8f7f7e979335c6ccd12
Solomon Ritzow <none>
2018-02-17 12:32:39 -0800

    Removed pointless BaseServer code

--- BaseStation/BaseServer/BaseServer.cs
+++ BaseStation/BaseServer/BaseServer.cs
@@ -2,18 +2,10 @@
 
 namespace HuskyRobotics.BaseStation.Server {
     /// <summary>
-    /// Contains functionality to communicate with the client/rover. 
-    /// Acts as an abstraction over Scarlet networking APIs.
+    /// Contains functionality to communicate with the client/rover.
+    /// Acts as an abstraction over Scarlet communications API.
     /// </summary>
-    public class BaseServer {
-        private readonly IPEndPoint address;
+    public static class BaseServer {
 
-        /// <summary>
-        /// Construct a BaseServer with the provided bind socket (ip address and port)
-        /// </summary>
-        /// <param name="bindAddress"></param>
-        public BaseServer(IPEndPoint bindAddress) {
-            this.address = bindAddress;
-        }
     }
 }
