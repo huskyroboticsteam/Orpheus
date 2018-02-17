1019d5162ed6305276293eb55c2bd945e34e288f
Emela Moreyra <emelavm@gmail.com>
2018-02-17 13:43:40 -0800

    Both client and server setup for sending controller inputs over then network current controls sending are: left x and y axis

---
+++ BaseStation/Controller Controls/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenTK.Input;
+using Scarlet.Communications;
+using Scarlet.Utilities;
+
+namespace ControllerServer
+{
+    class Program
+    {
+        private enum PacketID : byte {GenericPackets, LXAxis, LYAxis};
+        static void Main(string[] args)
+        {
+            int PortTCP = 5287;
+            int PortUDP = 5288;
+
+            Parse.SetParseHandler((byte)PacketID.GenericPackets, PrintPacketData);
+
+            Log.SetGlobalOutputLevel(Log.Severity.ERROR);
+            Server.Start(PortTCP, PortUDP);
+            while (true)
+            {
+                if (Server.GetClients().Contains("Controller Parser"))
+                {
+                    GamePadState state = GamePad.GetState(0);
+                    // Left X-axis
+                    sendPacket(new Message((byte)PacketID.LXAxis, UtilData.ToBytes(state.ThumbSticks.Left.X)),
+                        true, "Controller Parser");
+                    // Left Y-axis
+                    sendPacket(new Message((byte)PacketID.LYAxis, UtilData.ToBytes(state.ThumbSticks.Left.Y)),
+                        true, "Controller Parser");
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+        // sends a packet with the given message udp setting and endpoint
+        private static void sendPacket(Message message, bool isUDP, String endpoint)
+        {
+            Packet MyPack = new Packet(message, isUDP, endpoint);
+            Server.Send(MyPack);
+        }
+
+        // package handler prints package contents to console
+        private static void PrintPacketData(Packet packet)
+        {
+            Console.WriteLine(UtilData.ToString(packet.Data.Payload));
+        }
+
+    }
+}
---
+++ BaseStation/Cotroller Client Test/Program.cs
@@ -0,0 +1,82 @@
+﻿using Scarlet.Communications;
+using Scarlet.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerClient
+{
+    class Program
+    {
+        static string ServerIP;
+        private enum PacketID : byte {GenericPackets, LXAxis, LYAxis};
+        static void Main(string[] args)
+        {
+            ServerIP = "127.0.0.1";
+            int PortTCP = 5287;
+            int PortUDP = 5288;
+            string ClientName = "Controller Parser";
+
+            Parse.SetParseHandler((byte)PacketID.LXAxis, LXAxisHandle);
+            Parse.SetParseHandler((byte)PacketID.LYAxis, LYAxisHandle);
+
+            Log.SetGlobalOutputLevel(Log.Severity.ERROR);
+            Client.Start(ServerIP, PortTCP, PortUDP, ClientName);
+        }
+
+        // sends a message back to server indicating the clients interpretation of the packet
+        // for left y axis packets
+        private static void LYAxisHandle(Packet Packet)
+        {
+            float data = UtilData.ToFloat(Packet.Data.Payload);
+            Console.WriteLine(data);
+            data = (float)Math.Round(data, 2);
+            if (data < -0.02 || data > 0.02)
+            {
+                String say = "Broken";
+                if (data < -0.02)
+                {
+                    say = "Move Backward";
+                }
+                else if (data > 0.02)
+                {
+                    say = "Move Foreward";
+                }
+                Message returns = new Message((byte)PacketID.GenericPackets, say);
+                sendPacket(returns, true, "Server");
+            }
+        }
+
+        // sends a message back to server indicating the clients interpretation of the packet
+        // for left x axis packets
+        private static void LXAxisHandle(Packet Packet)
+        {
+            float data = UtilData.ToFloat(Packet.Data.Payload);
+            Console.WriteLine(data);
+            data = (float)Math.Round(data, 2);
+            if (data < -0.02 || data > 0.02)
+            {
+                String say = "Broken";
+                if (data < -0.02)
+                {
+                    say = "Straif left";
+                }
+                else if (data > 0.02)
+                {
+                    say = "Straif right";
+                }
+                Message returns = new Message((byte)PacketID.GenericPackets, say);
+                sendPacket(returns, true, "Server");
+            }
+        }
+
+        // sends a packet to the server with the given message, udp setting and endpoint
+        private static void sendPacket(Message message, bool isUDP, String endpoint)
+        {
+            Packet returnMessage = new Packet(message, isUDP, endpoint);
+            Client.Send(returnMessage);
+        }
+    }
+}
