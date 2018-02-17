a1a21ed955903df1f8b8371e1a6a0d79ee00f050
Solomon Ritzow <none>
2018-02-17 12:37:18 -0800

    Updated UI project
    Removed unnecessary App.xaml
    Added visual console viewer to remove necessity of windows console.
    Minor updates to other code.

--- BaseStation/MainWindow/MainWindow.xaml.cs
+++ BaseStation/MainWindow/MainWindow.xaml.cs
@@ -1,37 +1,34 @@
 ﻿using System.Windows;
-using HuskyRobotics.Utilities;
 using System.Diagnostics;
 using System.IO;
+using System.Windows.Controls;
 
-namespace HuskyRobotics.UI
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
+namespace HuskyRobotics.UI {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+			WindowState = WindowState.Maximized;
+			DataContext = this;
+		}
 
-        public ObservableMap<float> Properties { get; } = new MockObservableMap();
-
-        public MainWindow()
-        {
-            InitializeComponent();
-
-            DataContext = this;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e) {
-            string puttyPath = @"C:\Program Files (x86)\PuTTY\putty.exe";
-
-            if (File.Exists(puttyPath)) {
-                var process = new Process();
-                process.StartInfo.FileName = puttyPath;
-                process.StartInfo.Arguments = "-ssh root@192.168.0.50";
-                process.Start();
-            } else {
-                MessageBox.Show("Could not find PuTTY. You will need to install putty, or launch it manually\n" +
-                        "Looking at: " + puttyPath);
-            }
-        }
+		private void PuTTY_Button_Click(object sender, RoutedEventArgs e) {
+			string puttyPath = @"C:\Program Files (x86)\PuTTY\putty.exe";
+			if (File.Exists(puttyPath)) {
+				var process = new Process();
+				process.StartInfo.FileName = puttyPath;
+				process.StartInfo.Arguments = "-ssh root@192.168.0.50";
+				process.Start();
+			} else {
+				//remove button if PuTTY is not found
+				DockPanel panel = this.FindName("dock") as DockPanel;
+				panel.Children.Remove(sender as UIElement);
+				//display error message
+				MessageBox.Show("Could not find PuTTY. You will need to install putty, or launch it manually\n" +
+						"Looking at: " + puttyPath);
+			}
+		}
     }
 }
