04435b968481fcfe838c15e41c9e105096c1fedb
Solomon Ritzow <none>
2018-02-17 12:34:30 -0800

    Refactored VideoStreamer
    Removed unnecessary App.xaml
    Renamed MainWindow to VideoStream

---
+++ BaseStation/VideoStreamer/VideoStream.xaml.cs
@@ -0,0 +1,11 @@
+﻿using static LibVlcWrapper.LibVlcMethods;
+
+namespace HuskyRobotics.UI.VideoStreamer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class VideoDisplay : System.Windows.Controls.DockPanel {
+		public VideoDisplay() => InitializeComponent();
+	}
+}
