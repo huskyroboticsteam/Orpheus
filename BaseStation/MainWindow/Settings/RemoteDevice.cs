4fca7e21e1b8e103168e5b04fe9f13c4ffc7f6ef
Ethan Shea <ethan.shea1@gmail.com>
2018-02-19 14:21:15 -0800

    Settings Screen

---
+++ BaseStation/MainWindow/Settings/RemoteDevice.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuskyRobotics.UI
+{
+    [Serializable]
+    public class RemoteDevice : INotifyPropertyChanged
+    {
+        public RemoteDevice() : this("", "")
+        {
+        }
+
+        public RemoteDevice(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+
+        private string _name;
+        private string _address;
+
+        // Boilerplate
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name {
+            get => _name;
+            set {
+                _name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        public string Address {
+            get => _address;
+            set {
+                _address = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Address"));
+            }
+        }
+    }
+}
