f524830e1016b0adcf70683d1c599219b86cecda
Evan Higgins <evanh98@uw.edu>
2018-03-01 14:00:23 -0800

    Implemented a new library making graphing easier and cleaner.

--- BaseStation/SensorDiagnostics/SensorDiagnostics/LogSensors.cs
+++ BaseStation/SensorDiagnostics/SensorDiagnostics/LogSensors.cs
@@ -13,14 +13,16 @@ namespace FlatRobot
 {
     static class LogSensors
     {
-        private static List<DataLog> outputs = new List<DataLog>();
+        private static List<DataLog> outputs = new List<Datalog>();
         public static void logSensors()
         {
             Dictionary<string, int> sensorCount = new Dictionary<string, int>();
-            for (int i = 0; i < Global.sensors.length; i++)
+            ISensor s;
+            for (int i = 0; i < Global.sensors.Count; i++)
             {
+                s = Global.sensors[i];
                 if (!sensorCount.Keys.Contains(s.GetType().Name)) { sensorCount.Add(s.GetType().Name, 0); }
-                if (i > outputs.length) { outputs.Add(new DataLog(DateTime.Today.ToString() + "_" + s.GetType().Name + "_" + sensorCount[s.GetType().Name])); }
+                if (i > outputs.Count) { outputs.Add(new DataLog(DateTime.Today.ToString() + "_" + s.GetType().Name + "_" + sensorCount[s.GetType().Name])); }
                 outputs[i].Output(s.GetData());
                 sensorCount[s.GetType().Name]++;
             }
--- BaseStation/SensorDiagnostics/SensorDiagnostics/MainWindow.xaml.cs
+++ BaseStation/SensorDiagnostics/SensorDiagnostics/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,75 +26,40 @@ namespace HuskyRobotics.UI
     /// </summary>
     public partial class sensorHistoryGraph : UserControl
     {
+
+
+
+        public SeriesCollection seriesCollection { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+        public Func<double, string> XFormatter { get; set; }
         private const string LogFilesLocation = "Logs";
         private const char Delimiter = ',';
-        private const double margin = 10;
-        private const double xmin = margin;
-        private double xmax;
-        private const double ymin = margin;
-        private double ymax;
-        private const double step = 10;
-
         private int selectedFile;
         private FileInfo[] data;
-        private double maxValue = 0;
 
         public sensorHistoryGraph()
         {
             InitializeComponent();
+            seriesCollection = new SeriesCollection();
             getData();
             makeDropBox();
-            makeGraph();
+            YFormatter = valueY => valueY.ToString("0.##");
+            YFormatter = valueX => valueX.ToString("0.##");
         }
 
         private void getData()
         {
             DirectoryInfo di = new DirectoryInfo(LogFilesLocation);
-            data = di.GetFiles("*.csv");
-        }
-
-        private void makeGraph()
-        {
-
-            xmax = canGraph.Width - margin;
-            ymax = canGraph.Height - margin;
-
-            // Make the X axis.
-            GeometryGroup xaxis_geom = new GeometryGroup();
-            xaxis_geom.Children.Add(new LineGeometry(
-                new Point(0, ymax), new Point(canGraph.Width, ymax)));
-            for (double x = xmin + step;
-                x <= canGraph.Width - step; x += step)
+            try
             {
-                xaxis_geom.Children.Add(new LineGeometry(
-                    new Point(x, ymax - margin / 2),
-                    new Point(x, ymax + margin / 2)));
+                data = di.GetFiles("*.csv");
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("Logs Directory could not be found");
+                Console.Write(e);
             }
 
-            System.Windows.Shapes.Path xaxis_path = new System.Windows.Shapes.Path();
-            xaxis_path.StrokeThickness = 1;
-            xaxis_path.Stroke = Brushes.Black;
-            xaxis_path.Data = xaxis_geom;
-
-            canGraph.Children.Add(xaxis_path);
-
-            // Make the Y ayis.
-            GeometryGroup yaxis_geom = new GeometryGroup();
-            yaxis_geom.Children.Add(new LineGeometry(
-                new Point(xmin, 0), new Point(xmin, canGraph.Height)));
-            for (double y = step; y <= canGraph.Height - step; y += step)
-            {
-                yaxis_geom.Children.Add(new LineGeometry(
-                    new Point(xmin - margin / 2, y),
-                    new Point(xmin + margin / 2, y)));
-            }
-
-            System.Windows.Shapes.Path yaxis_path = new System.Windows.Shapes.Path();
-            yaxis_path.StrokeThickness = 1;
-            yaxis_path.Stroke = Brushes.Black;
-            yaxis_path.Data = yaxis_geom;
-
-            canGraph.Children.Add(yaxis_path);
         }
 
         private void makeDropBox()
@@ -107,51 +74,34 @@ namespace HuskyRobotics.UI
 
         private void updateGraph()
         {
+            seriesCollection.Clear();
             StreamReader sr = data[selectedFile].OpenText();
             string[] headers = sr.ReadLine().Split(Delimiter);
             List<double>[] sensorValues = new List<double>[headers.Length];
+            for (int i = 0; i < headers.Length; i++)
+            {
+                sensorValues[i] = new List<double>();
+            }
             while (sr.Peek() >= 0)
             {
                 string[] sensorLogData = sr.ReadLine().Split(Delimiter);
                 double currentdata;
-                for(int i=0; i<sensorLogData.Length; i++)
+                for (int i = 0; i < sensorLogData.Length; i++)
                 {
                     currentdata = Convert.ToDouble(sensorLogData[i]);
-                    if (maxValue < currentdata)
-                    {
-                        maxValue = currentdata;
-                    }
                     sensorValues[i].Add(currentdata);
                 }
-                
             }
-            Polyline polyline;
-            polyline = new Polyline();
-            polyline.StrokeThickness = 1;
-            int R = 128, G = 0, B = 128;
-            SolidColorBrush brushColor = new SolidColorBrush(Color.FromArgb(255, (byte)R, (byte)G, (byte)B));
-            PointCollection[] points = new PointCollection[headers.Length];
-            Point temp=new Point();
-            for(int i=0; i<headers.Length;i++)
-            {
-                foreach (double d in sensorValues[i])
+
+            for (int i=0; i < sensorValues.Length; i++) {
+                seriesCollection.Add(new LineSeries
                 {
-                    temp = new Point(ymax * (d / maxValue), i * (xmax / sensorValues[i].Count));
-                    points[i].Add(temp);
-                }
-                polyline.Stroke = brushColor;
-                polyline.Points = points[i];
-                canGraph.Children.Add(polyline);
-                Label lineLabel = new Label();
-                lineLabel.Content = headers[i];
-                canGraph.Children.Add(lineLabel);
-                Canvas.SetBottom(lineLabel,temp.Y);
-                Canvas.SetRight(lineLabel,temp.X);
-                canGraph.Children.Add(lineLabel);
-                R += 20;
-                B += 20;
-                brushColor.Color = Color.FromArgb(255, (byte)R, (byte)G, (byte)B);
+                    Title = headers[i],
+                    Values = new ChartValues<double>(sensorValues[i])
+                });
             }
+            Console.Write("test");
+            DataContext = this;
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
