9502751af9d9b74518883e4fb7efd9184d80a3ce
Ethan Shea <ethan.shea1@gmail.com>
2018-03-03 13:27:31 -0800

    Basic Arm Viewer Panel

--- BaseStation/MainWindow/ArmSideView.xaml.cs
+++ BaseStation/MainWindow/ArmSideView.xaml.cs
@@ -23,10 +23,19 @@ namespace HuskyRobotics.UI
     /// </summary>
     public partial class ArmSideView : UserControl
     {
-        private ObservableCollection<double> SetpointLocationX { get; set; }
-        private ObservableCollection<double> SetpointLocationY { get; set; }
+        public ArmSideView()
+        {
+            InitializeComponent();
 
-        public double Scale { get; } = 5;
+            DataContext = this;
+            SizeChanged += Resized;
+            PreviewMouseMove += MouseMotion; ;
+        }
+        
+        public ObservableCollection<double> SetpointLocationX { get; private set; }
+        public ObservableCollection<double> SetpointLocationY { get; private set; }
+
+        public double Scale { get; } = 1;
         
         private Armature _setpointArmature;
         public Armature SetpointArmature {
@@ -34,36 +43,47 @@ namespace HuskyRobotics.UI
             set {
                 if (_setpointArmature != null)
                 {
-                    _setpointArmature.PropertyChanged -= UpdateSetpoint;
+                    _setpointArmature.PropertyChanged -= UpdateSetpointArmature;
                 }
                 _setpointArmature = value;
-                _setpointArmature.PropertyChanged += UpdateSetpoint;
+                _setpointArmature.PropertyChanged += UpdateSetpointArmature;
                 SetpointLocationX = new ObservableCollection<double>(new List<double>(new double[_setpointArmature.Params.Length + 1]));
                 SetpointLocationY = new ObservableCollection<double>(new List<double>(new double[_setpointArmature.Params.Length + 1]));
                 RefreshLocations();
             }
         }
-
-        public ArmSideView()
-        {
-            InitializeComponent();
-        }
-
+        
         private void RefreshLocations()
         {
-            SetpointLocationX[0] = Width / 2;
-            SetpointLocationY[0] = Height / 2;
+            // Canvas is 100x100
+            SetpointLocationX[0] = 100 / 2;
+            SetpointLocationY[0] = 100 / 2;
 
             double angleSum = 0.0;
-            for(int i = 0; i < SetpointArmature.Params.Length; i++)
+            for (int i = 0; i < SetpointArmature.Params.Length; i++)
             {
                 angleSum += SetpointArmature.CurrentPitches[i];
                 SetpointLocationX[1 + i] = SetpointLocationX[i] + SetpointArmature.Params[i].Length * Scale * Math.Cos(angleSum);
-                SetpointLocationY[1 + i] = SetpointLocationY[i] + SetpointArmature.Params[i].Length * Scale * Math.Sin(angleSum);
+                SetpointLocationY[1 + i] = SetpointLocationY[i] - SetpointArmature.Params[i].Length * Scale * Math.Sin(angleSum);
             }
         }
+
+
+        private void MouseMotion(object sender, MouseEventArgs e)
+        {
+            Point location = e.GetPosition(Canvas);
+            location.X = (location.X - 50) / Scale;
+            location.Y = (location.Y - 50) / -Scale;
+
+            SetpointArmature.MoveTo((float)location.X, (float)location.Y, 0);
+        }
+
+        private void UpdateSetpointArmature(object sender, PropertyChangedEventArgs e)
+        {
+            RefreshLocations();
+        }
         
-        private void UpdateSetpoint(object sender, PropertyChangedEventArgs e)
+        private void Resized(object sender, SizeChangedEventArgs e)
         {
             RefreshLocations();
         }
