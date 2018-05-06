3c738bf7d44526bcdbfe6926e80a25a0afe2258a
Cai Biesinger <cai.biesinger@outlook.com>
2018-05-05 21:12:55 -0700

    Significantly improved music playing code.

--- Science/Science Onboard/Systems/MusicPlayer.cs
+++ Science/Science Onboard/Systems/MusicPlayer.cs
@@ -14,73 +14,63 @@ namespace Science.Systems
 {
     public class MusicPlayer : ISubsystem
     {
+        public static string MIDIFileName;
+        public static int OctaveShift = int.MinValue;
+
+        private const float MOTOR_FREQUENCY_SLOPE = 711.67F;
+        private const float MOTOR_FREQUENCY_INTERCEPT = 1.1111F;
+
+        private Thread PlayerThread;
         private MidiFile MIDI;
 
-        public void EmergencyStop()
+        public void EmergencyStop() { this.PlayerThread.Abort(); }
+
+        public void EventTriggered(object Sender, EventArgs Event) { }
+
+        public void UpdateState() { }
+
+        private void StartPlayer()
         {
-            
+            this.MIDI = MidiFile.Read(MIDIFileName);
+            IEnumerable<Note> Notes = this.MIDI.GetNotes();
+            Log.Output(Log.Severity.INFO, Log.Source.MOTORS, "Will play MIDI file " + MIDIFileName + ", shifted " + OctaveShift + " octaves. Found " + Notes.Count() + " notes.");
+            long PlayerPositionUS = 0;
+            Notes = Notes.OrderByDescending((Note n) => n.NoteNumber).OrderBy((Note n) => n.Time); // Stable sorts. Sorts notes to be in high-to-low order, then back to by-time, so that for chords, only the highest note gets played.
+            TempoMap TempoMap = this.MIDI.GetTempoMap();
+
+            Log.Output(Log.Severity.INFO, Log.Source.MOTORS, "MIDI playback starting...");
+            foreach (Note Note in Notes)
+            {
+                long StartUS = Note.TimeAs<MetricTimeSpan>(TempoMap).TotalMicroseconds;
+                long LengthUS = Note.LengthAs<MetricTimeSpan>(TempoMap).TotalMicroseconds;
+                if (StartUS < PlayerPositionUS) { continue; } // If we've missed the start of the note (due to overlap), skip it.
+                if (StartUS > (PlayerPositionUS + 1000)) { Thread.Sleep((int)((StartUS - PlayerPositionUS) / 1000)); } // If the start is in more than 1 ms, wait until then to allow for rests.
+
+                RoverMain.IOHandler.DrillController.SetSpeed(NoteToPercent(Note.NoteNumber + (OctaveShift * 12)), true);
+                Log.Output(Log.Severity.DEBUG, Log.Source.MOTORS, "Outputting note " + Note.NoteNumber + " at speed " + NoteToPercent(Note.NoteNumber + (OctaveShift * 12)) + " for " + (LengthUS / 1000) + "ms.");
+                Thread.Sleep((int)(LengthUS / 1000));
+                RoverMain.IOHandler.DrillController.SetSpeed(0, true);
+                PlayerPositionUS = StartUS + LengthUS; // The player is now at the end of the note we just played.
+            }
+            RoverMain.IOHandler.DrillController.SetSpeed(0, false);
+            Log.Output(Log.Severity.INFO, Log.Source.MOTORS, "MIDI playback complete.");
         }
-
-        public void EventTriggered(object Sender, EventArgs Event)
-        {
-            
-        }
-
-
 
         public void Initialize()
         {
-            this.MIDI = MidiFile.Read("music.mid");
-            IEnumerable<Note> Notes = this.MIDI.GetNotes();
-            Log.Output(Log.Severity.INFO, Log.Source.MOTORS, "Note count: " + Notes?.Count());
-            int Position = 0;
-
-            Notes = Notes.OrderByDescending((Note n) => n.NoteNumber).OrderBy((Note n) => n.Time);
-
-            bool DownOne = false;
-
-            Dictionary<string, Tuple<int, int>> NoteData = new Dictionary<string, Tuple<int, int>>();
-
-            TempoMap TMap = this.MIDI.GetTempoMap();
-
-            int Count = 0;
-            foreach (Note Note in Notes)
+            if (MIDIFileName != null && OctaveShift != int.MinValue)
             {
-                int Start = (int)(Note.TimeAs<MetricTimeSpan>(TMap).TotalMicroseconds / 1000);
-                int ms = (int)(Note.LengthAs<MetricTimeSpan>(TMap).TotalMicroseconds / 1000);
-                if (NoteData.ContainsKey(NoteToStr(Note))) { NoteData[NoteToStr(Note)] = new Tuple<int, int>(Start, ms); }
-                else { NoteData.Add(NoteToStr(Note), new Tuple<int, int>(Start, ms)); }
-                Count++;
-                if (Count % 100 == 0) { Log.Output(Log.Severity.INFO, Log.Source.MOTORS, "Now at " + Count); }
-            }
-
-            foreach (Note Note in Notes)
-            {
-                int Start = NoteData[NoteToStr(Note)].Item1;
-                int ms = NoteData[NoteToStr(Note)].Item2;
-                if (Start < Position) { continue; }
-                RoverMain.IOHandler.DrillController.SetSpeed(NoteToPercent(Note.NoteNumber - (DownOne ? 12 : 0)), true);
-                Log.Output(Log.Severity.INFO, Log.Source.MOTORS, "Outputting note " + Note.NoteNumber + " at freq " + NoteToPercent(Note.NoteNumber - (DownOne ? 12 : 0)) + " for " + ms + "ms.");
-                Thread.Sleep(ms);
-                RoverMain.IOHandler.DrillController.SetSpeed(0, false);
-                Position = Start + ms;
+                this.PlayerThread = new Thread(new ThreadStart(this.StartPlayer));
             }
         }
 
-        private static string NoteToStr(Note Note)
-        {
-            return Note.ToString() + Note.Time + "," + Note.Length;
-        }
+        private static string NoteToStr(Note Note) => Note.ToString() + Note.Time + "," + Note.Length;
 
         private static float NoteToPercent(int Note)
         {
-            double Frequency = 261.63 / Math.Pow((Math.Pow(2, 1F / 12)), 60 - Note);
-            return (float)((Frequency - 1.1111F) / 711.67F);
+            float Frequency = 261.63F / (float)(Math.Pow(Math.Pow(2, (1.0 / 12)), 60 - Note));
+            return (Frequency - MOTOR_FREQUENCY_INTERCEPT) / MOTOR_FREQUENCY_SLOPE;
         }
 
-        public void UpdateState()
-        {
-            
-        }
     }
 }
