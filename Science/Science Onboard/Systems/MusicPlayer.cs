79ab83e881c531d1bca15630041b80c679a51bb2
Cai Biesinger <cai.biesinger@outlook.com>
2018-05-05 21:15:42 -0700

    Some more music player improvements.

--- Science/Science Onboard/Systems/MusicPlayer.cs
+++ Science/Science Onboard/Systems/MusicPlayer.cs
@@ -21,7 +21,6 @@ namespace Science.Systems
         private const float MOTOR_FREQUENCY_INTERCEPT = 1.1111F;
 
         private Thread PlayerThread;
-        private MidiFile MIDI;
 
         public void EmergencyStop() { this.PlayerThread.Abort(); }
 
@@ -31,29 +30,37 @@ namespace Science.Systems
 
         private void StartPlayer()
         {
-            this.MIDI = MidiFile.Read(MIDIFileName);
-            IEnumerable<Note> Notes = this.MIDI.GetNotes();
-            Log.Output(Log.Severity.INFO, Log.Source.MOTORS, "Will play MIDI file " + MIDIFileName + ", shifted " + OctaveShift + " octaves. Found " + Notes.Count() + " notes.");
-            long PlayerPositionUS = 0;
-            Notes = Notes.OrderByDescending((Note n) => n.NoteNumber).OrderBy((Note n) => n.Time); // Stable sorts. Sorts notes to be in high-to-low order, then back to by-time, so that for chords, only the highest note gets played.
-            TempoMap TempoMap = this.MIDI.GetTempoMap();
-
-            Log.Output(Log.Severity.INFO, Log.Source.MOTORS, "MIDI playback starting...");
-            foreach (Note Note in Notes)
+            try
             {
-                long StartUS = Note.TimeAs<MetricTimeSpan>(TempoMap).TotalMicroseconds;
-                long LengthUS = Note.LengthAs<MetricTimeSpan>(TempoMap).TotalMicroseconds;
-                if (StartUS < PlayerPositionUS) { continue; } // If we've missed the start of the note (due to overlap), skip it.
-                if (StartUS > (PlayerPositionUS + 1000)) { Thread.Sleep((int)((StartUS - PlayerPositionUS) / 1000)); } // If the start is in more than 1 ms, wait until then to allow for rests.
+                MidiFile MIDI = MidiFile.Read(MIDIFileName);
+                IEnumerable<Note> Notes = MIDI.GetNotes();
+                Log.Output(Log.Severity.INFO, Log.Source.MOTORS, "Will play MIDI file " + MIDIFileName + ", shifted " + OctaveShift + " octaves. Found " + Notes.Count() + " notes.");
+                long PlayerPositionUS = 0;
+                Notes = Notes.OrderByDescending((Note n) => n.NoteNumber).OrderBy((Note n) => n.Time); // Stable sorts. Sorts notes to be in high-to-low order, then back to by-time, so that for chords, only the highest note gets played.
+                TempoMap TempoMap = MIDI.GetTempoMap();
 
-                RoverMain.IOHandler.DrillController.SetSpeed(NoteToPercent(Note.NoteNumber + (OctaveShift * 12)), true);
-                Log.Output(Log.Severity.DEBUG, Log.Source.MOTORS, "Outputting note " + Note.NoteNumber + " at speed " + NoteToPercent(Note.NoteNumber + (OctaveShift * 12)) + " for " + (LengthUS / 1000) + "ms.");
-                Thread.Sleep((int)(LengthUS / 1000));
-                RoverMain.IOHandler.DrillController.SetSpeed(0, true);
-                PlayerPositionUS = StartUS + LengthUS; // The player is now at the end of the note we just played.
+                Log.Output(Log.Severity.INFO, Log.Source.MOTORS, "MIDI playback starting...");
+                foreach (Note Note in Notes)
+                {
+                    long StartUS = Note.TimeAs<MetricTimeSpan>(TempoMap).TotalMicroseconds;
+                    long LengthUS = Note.LengthAs<MetricTimeSpan>(TempoMap).TotalMicroseconds;
+                    if (StartUS < PlayerPositionUS) { continue; } // If we've missed the start of the note (due to overlap), skip it.
+                    if (StartUS > (PlayerPositionUS + 1000)) { Thread.Sleep((int)((StartUS - PlayerPositionUS) / 1000)); } // If the start is in more than 1 ms, wait until then to allow for rests.
+
+                    RoverMain.IOHandler.DrillController.SetSpeed(NoteToPercent(Note.NoteNumber + (OctaveShift * 12)), true);
+                    Log.Output(Log.Severity.DEBUG, Log.Source.MOTORS, "Outputting note " + Note.NoteNumber + " at speed " + NoteToPercent(Note.NoteNumber + (OctaveShift * 12)) + " for " + (LengthUS / 1000) + "ms.");
+                    Thread.Sleep((int)(LengthUS / 1000));
+                    RoverMain.IOHandler.DrillController.SetSpeed(0, true);
+                    PlayerPositionUS = StartUS + LengthUS; // The player is now at the end of the note we just played.
+                }
+                RoverMain.IOHandler.DrillController.SetSpeed(0, false);
+                Log.Output(Log.Severity.INFO, Log.Source.MOTORS, "MIDI playback complete.");
             }
-            RoverMain.IOHandler.DrillController.SetSpeed(0, false);
-            Log.Output(Log.Severity.INFO, Log.Source.MOTORS, "MIDI playback complete.");
+            catch(Exception Exc)
+            {
+                Log.Output(Log.Severity.WARNING, Log.Source.MOTORS, "Failed to play MIDI file.");
+                Log.Exception(Log.Source.MOTORS, Exc);
+            }
         }
 
         public void Initialize()
