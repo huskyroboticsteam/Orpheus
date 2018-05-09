3234952ddf9a4d7327a22aac28427ddad261c541
Evan Higgins <evanh98@uw.edu>
2018-05-09 14:43:27 -0700

    Added extra PTZCamera constructors

--- BaseStation/PTZCamera/PTZCamera.cs
+++ BaseStation/PTZCamera/PTZCamera.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Http;
+using Scarlet;
+using Scarlet.Utilities;
 
 namespace HuskyRobotics
 {
@@ -25,13 +27,34 @@ namespace HuskyRobotics
             _base_url = string.Format("http://{0}/", camera_address);
         }
 
+        public PTZCamera(IPAddress camera_address, string username, string password, HttpClient client) : this(camera_address.ToString(),username,password,client){ }
+        public PTZCamera(string camera_address, string username, string password) : this(camera_address,username,password, new HttpClient()){ }
+        public PTZCamera(IPAddress camera_address, string username, string password) : this(camera_address.ToString(), username, password, new HttpClient()) { }
+
         public void _send_data(string relative_url, Dictionary<string, string> values)
         {
             List<string> data = new List<string>();
             foreach(KeyValuePair<string,string> entry in values)
             {
-
+                data.Add(entry.Key+"="+entry.Value);
             }
+            string url = this._base_url + relative_url + "?" + string.Join("&",data);
+            try { 
+                Task<string> responseString = client.GetStringAsync(url);
+            }
+            catch
+            {
+                Log.Output(Log.Severity.ERROR, Log.Source.CAMERAS, "Could not connect to camera at ip: "+_base_url);
+            }
+        }
+
+        public void set_speeds(int x_speed, int y_speed)
+        {
+            Dictionary<string, string> values = new Dictionary<string, string>()
+            {
+                {"continuouspantiltmove", string.Format("{0},{1}",x_speed,y_speed) }
+            };
+            this._send_data("ptz.cgi",values);
         }
     }
 }
