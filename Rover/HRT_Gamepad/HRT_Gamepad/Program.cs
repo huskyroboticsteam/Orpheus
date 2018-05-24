bc26fc0933a5c6d8a659a1e505c968e5ac62a56e
kwongab <kwongab@uw.edu>
2018-05-23 19:33:24 -0700

    supports joystick to drive rover

--- Rover/HRT_Gamepad/HRT_Gamepad/Program.cs
+++ Rover/HRT_Gamepad/HRT_Gamepad/Program.cs
@@ -42,80 +42,107 @@ namespace Minibot
             var client = new UdpClient();
             IPEndPoint ep = new IPEndPoint(IPAddress.Parse("192.168.0.20"), 9000);
             client.Connect(ep);*/
-
+            bool usingJoystick = false;
             Client.Start("192.168.0.20", 1025, 1026, "Client 1");
-
-            int count = 0;
-            while (true)
+            if (usingJoystick)
             {
-
-                GamePadState State = GamePad.GetState(0);
-                if (State.Buttons.A == ButtonState.Pressed)
+                while (true)
+                // for (int i = 0; i < 4; i++)
                 {
-                    do
+                    //JoystickState state = new JoystickState();
+                    var state = Joystick.GetState(0);
+                    if (state.IsConnected)
                     {
-                        Thread.Sleep(500);
-                        count++;
-                        State = GamePad.GetState(0);
-                        //Console.WriteLine("Reading");
-                        if (State.IsConnected)
-                        {
-                            float rightSpeed = State.Triggers.Right;
-                            float leftSpeed = State.Triggers.Left;
-                            float speed = rightSpeed - leftSpeed;
+                        float x = state.GetAxis(0);
+                        float y = state.GetAxis(1);
 
-                            float leftJoy = State.ThumbSticks.Left.Y;
-                            float rightJoy = State.ThumbSticks.Right.X + 0.003967406f;
+                        // Print the current state of the joystick
+                        //Console.WriteLine(state);
+                        Console.WriteLine("X: " + x + "Y: " + -y);
 
-                            
-                            //Console.WriteLine("Left Trigger: " + leftSpeed);
-                            //Console.WriteLine("Right Trigger: " + rightSpeed);
-                            float turn = rightSpeed - leftSpeed;
+                        string stringData = x + "," + -y;
 
-                            if (count == 100)
-                            {
-                                Console.WriteLine($"Speed: {rightJoy}");
-                                Console.WriteLine($"Turn: {turn}");
-                                Console.WriteLine();
-                                count = 0;
-                            }
+                        //byte [] sendBytes = Encoding.ASCII.GetBytes(stringData);
+                        //client.Send(sendBytes, sendBytes.Length);
 
-                            string stringData = turn + "," + rightJoy;
-
-                            //byte [] sendBytes = Encoding.ASCII.GetBytes(stringData);
-                            //client.Send(sendBytes, sendBytes.Length);
-
-                            
-                            Console.WriteLine(stringData);
-
-
-                            Packet MyPack = new Packet(0x30, false);
-                            MyPack.AppendData(UtilData.ToBytes(stringData));
-                            MyPack.IsUDP = false;
-                            Client.Send(MyPack);
-                            
-
-                            
-
-                            /*
-                            canName.Write(5, UtilData.ToBytes((int)0.15*turn*100000.0)); 
-                             
-                            canName.Write(1, UtilData.ToBytes((int)rightJoy * 100000.0));
-                            canName.Write(2, UtilData.ToBytes((int)rightJoy * 100000.0));
-                            canName.Write(3, UtilData.ToBytes((int)rightJoy * 100000.0));
-                            canName.Write(4, UtilData.ToBytes((int)rightJoy * 100000.0));
-                            */
-                        }
-                        else
-                        {
-                            Console.WriteLine("NOT CONNECTED");
-                        }
+                        Packet MyPack = new Packet(0x30, false);
+                        MyPack.AppendData(UtilData.ToBytes(stringData));
+                        MyPack.IsUDP = false;
+                        Client.Send(MyPack);
                     }
-                    while (State.Buttons.Start != ButtonState.Pressed && State.Buttons.B != ButtonState.Pressed);
                 }
+            } 
+            else
+            { 
+                int count = 0;
+                while (true)
+                {
 
-                
+                    GamePadState State = GamePad.GetState(0);
+                    if (State.Buttons.A == ButtonState.Pressed)
+                    {
+                        do
+                        {
+                            //Thread.Sleep(500);
+                            count++;
+                            State = GamePad.GetState(0);
+                            //Console.WriteLine("Reading");
+                            if (State.IsConnected)
+                            {
+                                float rightSpeed = State.Triggers.Right;
+                                float leftSpeed = State.Triggers.Left;
+                                float speed = rightSpeed - leftSpeed;
 
+                                float leftJoy = State.ThumbSticks.Left.Y;
+                                float rightJoy = State.ThumbSticks.Right.X + 0.003967406f;
+
+
+                                //Console.WriteLine("Left Trigger: " + leftSpeed);
+                                //Console.WriteLine("Right Trigger: " + rightSpeed);
+                                float turn = rightSpeed - leftSpeed;
+
+                                if (count == 100)
+                                {
+                                    Console.WriteLine($"Speed: {rightJoy}");
+                                    Console.WriteLine($"Turn: {turn}");
+                                    Console.WriteLine();
+                                    count = 0;
+                                }
+
+                                string stringData = turn + "," + rightJoy;
+
+                                //byte [] sendBytes = Encoding.ASCII.GetBytes(stringData);
+                                //client.Send(sendBytes, sendBytes.Length);
+
+
+                                Console.WriteLine(stringData);
+
+
+                                Packet MyPack = new Packet(0x30, false);
+                                MyPack.AppendData(UtilData.ToBytes(stringData));
+                                MyPack.IsUDP = false;
+                                Client.Send(MyPack);
+
+
+
+
+                                /*
+                                canName.Write(5, UtilData.ToBytes((int)0.15*turn*100000.0)); 
+                             
+                                canName.Write(1, UtilData.ToBytes((int)rightJoy * 100000.0));
+                                canName.Write(2, UtilData.ToBytes((int)rightJoy * 100000.0));
+                                canName.Write(3, UtilData.ToBytes((int)rightJoy * 100000.0));
+                                canName.Write(4, UtilData.ToBytes((int)rightJoy * 100000.0));
+                                */
+                            }
+                            else
+                            {
+                                Console.WriteLine("NOT CONNECTED");
+                            }
+                        }
+                        while (State.Buttons.Start != ButtonState.Pressed && State.Buttons.B != ButtonState.Pressed);
+                    }
+                }
             }
         }
     }
