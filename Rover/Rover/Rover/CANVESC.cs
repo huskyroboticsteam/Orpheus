400bf6b589f7dc3244320c902b233e8dc1597f81
Benton Kwong <kwongbenton@outlook.com>
2018-05-23 19:37:03 -0700

    Updates to VESC object

---
+++ Rover/Rover/Rover/CANVESC.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Scarlet.Filters;
+using Scarlet.IO;
+using Scarlet.Utilities;
+using System.Threading;
+using System.Collections.Generic;
+using Scarlet.Components;
+
+namespace Scarlet.Components.Motors
+{
+    public class CANVESC : IMotor
+    {
+        private IFilter<float> Filter; // Filter for speed output
+        private readonly ICANBus CANBus;
+        private readonly float MaxSpeed;
+        private readonly long CanID;
+
+        private bool OngoingSpeedThread; // Whether or not a thread is running to set the speed
+        private bool Stopped; // Whether or not the motor is stopped
+        public float TargetSpeed { get; private set; } // Target speed (-1.0 to 1.0) of the motor
+
+        /// <summary> Initializes a VESC Motor controller </summary>
+        /// <param name="CANBus"> CAN output to control the motor controller </param>
+        /// <param name="MaxSpeed"> Limiting factor for speed (should never exceed + or - this val) </param>
+        /// <param name="CANForwardID"> CAN ID of the motor controller (-1 to disable CAN forwarding) </param>
+        /// <param name="SpeedFilter"> Filter to use with MC. Good for ramp-up protection and other applications </param>
+        public CANVESC(ICANBus CANBus, long CanID, float MaxSpeed, IFilter<float> SpeedFilter = null)
+        {
+            this.CANBus = CANBus;
+            this.CanID = CanID;
+            this.MaxSpeed = Math.Abs(MaxSpeed);
+            this.Filter = SpeedFilter;
+            this.SetSpeedDirectly(0.0f);
+        }
+
+        public void EventTriggered(object Sender, EventArgs Event) { }
+
+        /// <summary> 
+        /// Immediately sets the enabled status of the motor.
+        /// Stops the motor if given parameter is false.
+        /// Does not reset the target speed to zero, so beware
+        /// of resetting this to enabled.
+        /// </summary>
+        public void SetEnabled(bool Enabled)
+        {
+            this.Stopped = !Enabled;
+            if (Enabled) { this.SetSpeed(this.TargetSpeed); }
+            else { this.SetSpeedDirectly(0); }
+        }
+
+        /// <summary> Sets the speed on a thread for filtering. </summary>
+        private void SetSpeedThread()
+        {
+            float Output = this.Filter.GetOutput();
+            while (!this.Filter.IsSteadyState())
+            {
+                if (Stopped) { SetSpeedDirectly(0); }
+                else
+                {
+                    this.Filter.Feed(this.TargetSpeed);
+                    SetSpeedDirectly(this.Filter.GetOutput());
+                }
+                Thread.Sleep(Constants.DEFAULT_MIN_THREAD_SLEEP);
+            }
+            OngoingSpeedThread = false;
+        }
+
+        /// <summary> Creates a new thread for setting speed during motor filtering output </summary>
+        /// <returns> A new thread for changing the motor speed. </returns>
+        private Thread SetSpeedThreadFactory() { return new Thread(new ThreadStart(SetSpeedThread)); }
+
+        /// <summary>
+        /// Sets the motor speed. Output may vary from the given value under the following conditions:
+        /// - Input exceeds maximum speed. Capped to given maximum.
+        /// - Filter changes value. Filter's output used instead.
+        ///     (If filter is null, this does not occur)
+        /// - The motor is disabled. You must first re-enable the motor.
+        /// </summary>
+        /// <param name="Speed"> The new speed to set the motor at. From -1.0 to 1.0 </param>
+        public void SetSpeed(float Speed)
+        {
+            if (this.Filter != null && !this.Filter.IsSteadyState() && !OngoingSpeedThread)
+            {
+                this.Filter.Feed(Speed);
+                SetSpeedThreadFactory().Start();
+                OngoingSpeedThread = true;
+            }
+            else { SetSpeedDirectly(Speed); }
+            this.TargetSpeed = Speed;
+        }
+
+        /// <summary>
+        /// Sets the speed directly given an input from -1.0 to 1.0
+        /// Takes into consideration motor stop signal and max speed restriction.
+        /// </summary>
+        /// <param name="Speed"> Speed from -1.0 to 1.0 </param>
+        private void SetSpeedDirectly(float Speed)
+        {
+            if (Speed > this.MaxSpeed) { Speed = this.MaxSpeed; }
+            if (-Speed > this.MaxSpeed) { Speed = -this.MaxSpeed; }
+            if (this.Stopped) { Speed = 0; }
+            this.SendSpeed(Speed);
+        }
+
+        /// <summary>
+        /// Sends the speed between -1.0 and 1.0 to the motor controller
+        /// </summary>
+        /// <param name="Speed"> Speed from -1.0 to 1.0 </param>
+        private void SendSpeed(float Speed)
+        {
+            // Duty Cycle (100000.0 mysterious magic number from https://github.com/VTAstrobotics/VESC_BBB_UART/blob/master/bldc_interface.c)
+            this.CANBus.Write((uint)CanID, UtilData.ToBytes((Int32)(Speed * 100000.0)));
+        }
+       
+    }
+}
--- Rover/Rover/Rover/Program.cs
+++ Rover/Rover/Rover/Program.cs
@@ -30,41 +30,6 @@ namespace Rover
             Parse.SetParseHandler(0x30, messageParser);
 
             
-
-            /*
-            while (true)
-            {
-
-                //var data = udpServer.Receive(ref remoteEP);
-                
-
-                String dataString = Encoding.ASCII.GetString(data);
-
-                Console.WriteLine(dataString);
-
-                if (!String.IsNullOrEmpty(dataString))
-                {
-
-                    String[] speedDis = dataString.Split(',');
-                    Double steer = Convert.ToDouble(speedDis[0]);
-                    Double speed = Convert.ToDouble(speedDis[1]);
-
-                    if (Math.Abs(speed) < 0.01)
-                    {
-                        speed = 0;
-                    }
-
-                    Console.WriteLine(steer + "," + speed);
-                    
-                    canName.Write(1, UtilData.ToBytes((int)(speed * 100000.0)));
-                    canName.Write(2, UtilData.ToBytes((int)(speed * 100000.0)));
-                    canName.Write(3, UtilData.ToBytes((int)(speed * 100000.0)));
-                    canName.Write(4, UtilData.ToBytes((int)(-1*speed * 100000.0)));
-                    canName.Write(5, UtilData.ToBytes((int)(0.15 * steer * 100000.0)));
-                    Thread.Sleep(50);
-                }
-
-            }*/
         }
 
         public static void messageParser(Packet recivedData)
