1dfcfc205f43c2dd196240d5d9036845d560d3d0
Nathan <obbay2@hotmail.com>
2018-05-28 13:08:58 -0700

    Fix

--- BaseStation/BaseServer/BaseServer.cs
+++ BaseStation/BaseServer/BaseServer.cs
@@ -7,12 +7,14 @@ using System.Linq;
 using System.Threading;
 using System.Collections.Generic;
 
-namespace HuskyRobotics.BaseStation.Server {
+namespace HuskyRobotics.BaseStation.Server
+{
     /// <summary>
     /// Contains functionality to communicate with the client/rover.
     /// Acts as an abstraction over Scarlet communications API.
     /// </summary>
-    public static class BaseServer {
+    public static class BaseServer
+    {
 
         private static bool shutdown = false;
         private static Controller gamepad;
@@ -23,9 +25,16 @@ namespace HuskyRobotics.BaseStation.Server {
         public static void Start()
         {
             Scarlet.Communications.Server.Start(1025, 1026);
+            Scarlet.Communications.Server.ClientConnectionChange += memes;
             Parse.SetParseHandler(0xC0, gpsHandler);
             Parse.SetParseHandler(0xC1, magnetomerHandler);
             gamepad = GetGamepad();
+        }
+
+        private static void memes(object sender, EventArgs e)
+        {
+            Console.WriteLine("Clients Changed");
+            Console.WriteLine(Scarlet.Communications.Server.GetClients());
         }
 
         private static Controller GetGamepad()
@@ -36,33 +45,58 @@ namespace HuskyRobotics.BaseStation.Server {
 
         public static void EventLoop()
         {
-            while (!shutdown) {
+            while (!shutdown)
+            {
+                if (!gamepad.IsConnected)
+                {
+                    Console.WriteLine("Gamepad not connected");
+                    Thread.Sleep(100);
+                    continue;
+                }
                 State state = gamepad.GetState();
                 byte rightTrigger = state.Gamepad.RightTrigger;
                 byte leftTrigger = state.Gamepad.LeftTrigger;
                 short leftThumbX = state.Gamepad.LeftThumbX;
 
+                Console.WriteLine(leftThumbX);
+
+                if (leftThumbX == -32768)
+                {
+                    leftThumbX++;
+                }
+
                 if (rightTrigger < TriggerThreshold) { rightTrigger = 0; }
                 if (leftTrigger < TriggerThreshold) { leftTrigger = 0; }
-                if (leftThumbX < LeftThumbDeadzone) { leftThumbX = 0; }
+                if (Math.Abs(leftThumbX) < LeftThumbDeadzone) { leftThumbX = 0; }
 
                 float speed = rightTrigger - leftTrigger;
                 float steerPos = leftThumbX;
-                speed = (float) UtilMain.LinearMap(speed, -255, 255, -1, 1);
-                steerPos = (float)UtilMain.LinearMap(steerPos, -32768, 32767, -1, 1);
-                
-                //Console.WriteLine("Speed: " + speed);
-                //Console.WriteLine("Steer Pos: " + steerPos);
 
-                Packet SteerPack = new Packet(0x94, true);
-                SteerPack.AppendData(UtilData.ToBytes(steerPos));
+                speed = (float)UtilMain.LinearMap(speed, -255, 255, -1, 1);
+                steerPos = (float)UtilMain.LinearMap(steerPos, -32768, 32767, -1, 1);
+
+                Console.WriteLine("Speed: " + speed);
+                Console.WriteLine("Steer Pos: " + steerPos);
+
+                bool a = (state.Gamepad.Buttons & GamepadButtonFlags.A) != 0;
+                bool b = (state.Gamepad.Buttons & GamepadButtonFlags.B) != 0;
+                sbyte speed1 = 60;
+                sbyte packet = 0;
+                if (a)
+                    packet = speed1;
+                if (b)
+                    packet = (sbyte)-speed1;
+                Console.WriteLine(packet);
+                Packet SteerPack = new Packet(0x8F, true, "MainRover");
+                SteerPack.AppendData(new byte[] { (byte)packet });
+                //SteerPack.AppendData(UtilData.ToBytes(steerPos));
                 Scarlet.Communications.Server.Send(SteerPack);
 
-                Packet SpeedPack = new Packet(0x95, true);
+                Packet SpeedPack = new Packet(0x95, true, "MainRover");
                 SpeedPack.AppendData(UtilData.ToBytes(speed));
                 Scarlet.Communications.Server.Send(SpeedPack);
 
-                Thread.Sleep(10);
+                Thread.Sleep(100);
             }
         }
 
