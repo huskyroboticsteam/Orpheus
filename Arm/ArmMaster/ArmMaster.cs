4c5fe6a42040449c3608f7ff813315041096b2ef
save-buffer <warplane@hotmail.com>
2018-06-02 13:00:12 -0700

    Added useful things

--- Arm/ArmMaster/ArmMaster.cs
+++ Arm/ArmMaster/ArmMaster.cs
@@ -7,40 +7,84 @@ using Scarlet.IO;
 
 namespace ArmMaster
 {
-    public class ArmMaster
+    class ArmMaster
     {
         const string CHASSIS_BEAGLEBONE = "192.168.0.5";
-        public static void Main(string[] args)
+        static IUARTBus Slave;
+
+        static void SetupBeaglebone()
         {
+            Slave = UARTBBB.UARTBus1;
             StateStore.Start("ArmMaster");
             BeagleBone.Initialize(SystemMode.DEFAULT, true);
-            BBBPinManager.AddMappingUART(BBBPin.P9_24);
-            BBBPinManager.AddMappingUART(BBBPin.P9_26);
+        }
+
+        static void SetupPins()
+        {
+            BBBPinManager.AddMappingUART(Pins.SlaveRX);
+            BBBPinManager.AddMappingUART(Pins.SlaveTX);
+            BBBPinManager.AddMappingPWM(Pins.BaseRotation);
+            BBBPinManager.AddMappingGPIO(Pins.BaseRotationDir, true, ResistorState.NONE);
+            BBBPinManager.AddMappingPWM(Pins.Elbow);
+            BBBPinManager.AddMappingPWM(Pins.Shoulder);
+            BBBPinManager.AddMappingADC(Pins.ShoulderPot);
+            BBBPinManager.AddMappingGPIO(Pins.ElbowLimitSwitch, false, ResistorState.PULL_UP);
             BBBPinManager.ApplyPinSettings(BBBPinManager.ApplicationMode.APPLY_IF_NONE);
+        }
+
+        static void SetupClient()
+        {
             Client.Start(CHASSIS_BEAGLEBONE, 1025, 1026, "ArmMaster");
-            IUARTBus Slave = UARTBBB.UARTBus1;
             Parse.SetParseHandler(0x80, (Pack) =>
             {
+                MotorControl.SetAllMotorSpeed(0.0f);
                 Slave.Write(new byte[] { 0x80 });
             });
-            for (byte i = 0x9A; i <= 0xA0; i++)
+            for (byte i = 0x9A; i <= 0x9C; i++)
+            {
+                Parse.SetParseHandler(i, (Packet) =>
+                {
+                    MotorControl.SetMotorSpeed(Packet.Data.ID - 0x9A, UtilData.ToFloat(Packet.Data.Payload));
+                });
+
+            }
+            for (byte i = 0x9D; i <= 0xA0; i++)
             {
                 Parse.SetParseHandler(i, (Pack) =>
                 {
-                    Slave.Write(new byte[] { i });
+                    Slave.Write(new byte[] { Pack.Data.ID });
                     Slave.Write(Pack.Data.Payload);
                 });
             }
+        }
+
+        static void Main(string[] args)
+        {
+
+            SetupBeaglebone();
+            SetupPins();
+            SetupClient();
+
             const int PacketSize = sizeof(byte) + sizeof(int) + sizeof(int) + sizeof(int) + sizeof(byte);
             Packet p = new Packet(0xD3, true);
             byte[] ID = new byte[1];
             p.Data.Payload = new byte[PacketSize - 1];
             for (; ; )
             {
-                Slave.Read(1, ID);
+                /*Slave.Read(1, ID);
                 Slave.Read(PacketSize, p.Data.Payload);
                 p.Data.ID = ID[0];
-                Client.SendNow(p);
+                Client.SendNow(p);*/
+                string[] tokens = Console.ReadLine().Split(' ');
+                byte id = Byte.Parse(tokens[0], System.Globalization.NumberStyles.HexNumber);
+                float speed = float.Parse(tokens[1]);
+                if (0x9A <= id && id <= 0x9C)
+                    MotorControl.SetMotorSpeed(id - 0x9A, speed);
+                else
+                {
+                    Slave.Write(new byte[] { id });
+                    Slave.Write(UtilData.ToBytes(speed));
+                }
             }
 
         }
---
+++ Arm/ArmMaster/MotorControl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Scarlet.Filters;
+using Scarlet.IO.BeagleBone;
+using Scarlet.Components.Motors;
+
+namespace ArmMaster
+{
+    static class MotorControl
+    {
+        static CytronMD30C MotBaseRotation;
+        static TalonMC MotShoulder;
+        static TalonMC MotElbow;
+
+        public const int BaseRotation = 0;
+        public const int Shoulder = 1;
+        public const int Elbow = 2;
+
+        public static void Initialize()
+        {
+            MotBaseRotation = new CytronMD30C(PWMBBB.PWMDevice2.OutputA, new DigitalOutBBB(Pins.BaseRotationDir), 0.3f, new LowPass<float>());
+            MotShoulder = new TalonMC(PWMBBB.PWMDevice1.OutputA, 0.3f, new LowPass<float>());
+            MotElbow = new TalonMC(PWMBBB.PWMDevice1.OutputB, 0.3f, new LowPass<float>());
+        }
+
+        public static void SetMotorSpeed(int Motor, float Speed)
+        {
+            switch (Motor)
+            {
+                case BaseRotation:
+                    SetBaseSpeed(Speed);
+                    break;
+                case Shoulder:
+                    SetShoulderSpeed(Speed);
+                    break;
+                case Elbow:
+                    SetElbowSpeed(Speed);
+                    break;
+            }
+        }
+
+        public static void SetAllMotorSpeed(float Speed)
+        {
+            for (int i = 0; i < 2; i++)
+                SetMotorSpeed(i, Speed);
+        }
+
+        public static void SetBaseSpeed(float Speed)
+        {
+            MotBaseRotation.SetSpeed(Speed);
+        }
+
+        public static void SetShoulderSpeed(float Speed)
+        {
+            MotShoulder.SetSpeed(Speed);
+        }
+
+        public static void SetElbowSpeed(float Speed)
+        {
+            MotElbow.SetSpeed(Speed);
+        }
+    }
+}
