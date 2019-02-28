7a47b820993ed1d1eed6e84fffa8581c85b0dd49
Nathan Wreggit <obbay2@hotmail.com>
2019-02-27 20:04:36 -0800

    Multi quality

--- Rover/GStreamer/server.cpp
+++ Rover/GStreamer/server.cpp
@@ -7,7 +7,30 @@
 
 using namespace std;
 
-unordered_map<string, tuple<const char*, int, int, float, const char*>> table;
+unordered_map<string, tuple<const char*, int, int, vector<float>*, const char*>> table;
+
+void
+set_launch(GstRTSPServer * server, char * pipeline, const char * index)
+{
+  GstRTSPMountPoints *mounts;
+  GstRTSPMediaFactory *factory;
+ 
+  /* get the mount points for this server, every server has a default object
+   * that be used to map uri mount points to media factories */
+  mounts = gst_rtsp_server_get_mount_points (server);
+
+  factory = gst_rtsp_media_factory_new ();
+  gst_rtsp_media_factory_set_launch (factory, pipeline);
+  gst_rtsp_media_factory_set_shared (factory, TRUE);
+
+  /* attach the test factory to the /feed + i url */
+  string attach = "feed";
+  attach += index;
+  gst_rtsp_mount_points_add_factory (mounts, attach.c_str(), factory);
+ 
+  /* don't need the ref to the mapper anymore */
+  g_object_unref (mounts); 
+}
 
 char *
 construct_pipeline(char * devpath, const char * input_type, int width, int height, float scale)
@@ -28,11 +51,11 @@ construct_pipeline(char * devpath, const char * input_type, int width, int heigh
 
   if (strcmp(input_type, "I420") != 0)
   {
-    sprintf(output, "v4l2src device=%s ! video/x-raw, format=%s, width=%s, height=%s ! videoconvert ! video/x-raw, format=I420, width=%s, height=%s ! videoscale ! video/x-raw, format=I420, width=%s, height=%s ! rtpvrawpay name=pay0 pt=96", devpath, input_type, width_str, height_str, width_str, height_str, scaled_width_str, scaled_height_str);
+    sprintf(output, "intervideosrc ! video/x-raw, format=%s, width=%s, height=%s ! videoconvert ! video/x-raw, format=I420, width=%s, height=%s ! videoscale ! video/x-raw, format=I420, width=%s, height=%s ! rtpvrawpay name=pay0 pt=96", input_type, width_str, height_str, width_str, height_str, scaled_width_str, scaled_height_str);
   }
   else
   {
-    sprintf(output, "v4l2src device=%s ! video/x-raw, format=I420, width=%s, height=%s ! videoscale ! video/x-raw, format=I420, width=%s, height=%s ! rtpvrawpay name=pay0 pt=96", devpath, width_str, height_str, scaled_width_str, scaled_height_str);
+    sprintf(output, "intervideosrc ! video/x-raw, format=I420, width=%s, height=%s ! videoscale ! video/x-raw, format=I420, width=%s, height=%s ! rtpvrawpay name=pay0 pt=96", width_str, height_str, scaled_width_str, scaled_height_str);
   }
 
 
@@ -42,20 +65,23 @@ construct_pipeline(char * devpath, const char * input_type, int width, int heigh
 void
 setup_map()
 {
-  table["ZED"] = make_tuple("5556", 4416, 1242, 0.25f, "YUY2");
+  vector<float> * zedlist = new vector<float>;
+  zedlist->push_back(0.25f);
+  zedlist->push_back(0.1f);
+  table["ZED"] = make_tuple("5556", 4416, 1242, zedlist, "YUY2");
+  
 }
 
 int
 main (int argc, char *argv[])
 {
   GMainLoop *loop;
+  GstElement *inputpipe;
   GstRTSPServer *server;
-  GstRTSPMountPoints *mounts;
-  GstRTSPMediaFactory *factory;
   char * devname;
   char * devpath;
   const char * port;
-  char * pipeline;
+  vector<char *> pipelines;
   setup_map();
   gst_init(&argc, &argv);
   
@@ -70,8 +96,28 @@ main (int argc, char *argv[])
     }
     else
     {
-      pipeline = construct_pipeline(devpath, get<4>(table.at(devname)), get<1>(table.at(devname)), get<2>(table.at(devname)), get<3>(table.at(devname)));
-      port = get<0>(table.at(devname));
+      string input = "v4l2src device=";
+      input += devpath;
+      input += " ! intervideosink";
+      GstElement * inputpipe = gst_parse_launch(input.c_str(), NULL);
+      int ret = gst_element_set_state(inputpipe, GST_STATE_PLAYING);
+
+      if (ret == GST_STATE_CHANGE_FAILURE)
+      {
+        exit(-1);
+      }
+
+      tuple<const char*, int, int, vector<float>*, const char*> item = table.at(devname);
+      g_print("Hello");
+      for (int i = 0; i < (int) get<3>(item)->size(); i++)
+      {
+        pipelines.push_back(construct_pipeline(devpath, get<4>(item), get<1>(item), get<2>(item), (*(get<3>(item)))[i]));
+        #ifdef DEBUG
+          g_print("%s@%s > %s\n", devname, devpath, pipelines[i]);
+        #endif
+      }
+      
+      port = get<0>(item);
     }
   } 
   else if (argc == 5) // Likely from command line where everything is defined as an argument
@@ -79,7 +125,7 @@ main (int argc, char *argv[])
     devname = argv[1];
     devpath = argv[2];
     port = argv[3];
-    pipeline = argv[4];
+    pipelines.push_back(argv[4]);
   }
   else
   {
@@ -87,37 +133,26 @@ main (int argc, char *argv[])
     return -1;
   }
 
-  g_print("%s\n", pipeline);
-
   loop = g_main_loop_new (NULL, FALSE);
 
   /* create a server instance */
   server = gst_rtsp_server_new ();
   g_object_set (server, "service", port, NULL);
 
-  /* get the mount points for this server, every server has a default object
-   * that be used to map uri mount points to media factories */
-  mounts = gst_rtsp_server_get_mount_points (server);
-
   /* make a media factory for a test stream. The default media factory can use
    * gst-launch syntax to create pipelines.
    * any launch line works as long as it contains elements named pay%d. Each
    * element with pay%d names will be a stream */
-  factory = gst_rtsp_media_factory_new ();
-  gst_rtsp_media_factory_set_launch (factory, pipeline);
-  gst_rtsp_media_factory_set_shared (factory, TRUE);
-
-  /* attach the test factory to the /test url */
-  gst_rtsp_mount_points_add_factory (mounts, "/feed", factory);
-
-  /* don't need the ref to the mapper anymore */
-  g_object_unref (mounts);
+  for (int i = 0; i < (int) pipelines.size(); i++)
+  {
+    set_launch(server, pipelines[i], to_string(i).c_str());  
+    g_print ("%s@%s > stream ready at rtsp://127.0.0.1:%s/feed%d\n", devname, devpath, port, i);
+  }
 
   /* attach the server to the default maincontext */
   gst_rtsp_server_attach (server, NULL);
 
   /* start serving */
-  g_print ("%s@%s > stream ready at rtsp://127.0.0.1:%s/test\n", devname, devpath, port);
   g_main_loop_run (loop);
   g_main_loop_unref(loop);
 
