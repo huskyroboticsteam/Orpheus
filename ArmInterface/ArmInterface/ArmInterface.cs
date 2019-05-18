658ce4bb1113604c8e9c113b304778c735a2d40e
Baldstrom <jadenbottemiller@gmail.com>
2019-05-18 13:59:10 -0700

    Added initial Arm Interface Code

---
+++ ArmInterface/ArmInterface/ArmInterface.cs
@@ -0,0 +1,104 @@
+﻿using Scarlet.IO;
+using Scarlet.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ArmInterface
+{
+
+    public struct Packet
+    {
+        /// <summary> When sending, it's the receiver. When receiving, it's the sender. </summary>
+        public Device TargetDeviceID;
+        public bool Priority;
+        public CANPacket PacketType;
+        public byte[] Payload;
+    }
+
+    public class ArmInterface
+    {
+        public static byte DEVICE_ADDR = 0x02;
+
+        private bool _readEnable = true;
+
+        public bool ReadEnable
+        {
+            get { return _readEnable; }
+            set
+            {
+                if (value == true && _readEnable == false)
+                {
+                    ReadThread?.Start();
+                }
+                _readEnable = value;
+            }
+        }
+
+        private readonly ICANBus CANBus;
+        private readonly Thread ReadThread;
+
+        private Queue<Packet> ReceiveQueue;
+
+        public ArmInterface(ICANBus CANBus)
+        {
+            ReceiveQueue = new Queue<Packet>();
+            this.CANBus = CANBus;
+            // Start Read
+            ReadThread = new Thread(new ThreadStart(ReadOnThread));
+            ReadThread.Start();
+        }
+
+        public void Send(Packet SendPacket)
+        {
+            byte[] id = ConstructCanID(SendPacket);
+            uint constructedID = (uint)(id[2] << 8) | (uint)(id[1] << 4) | id[0];
+            CANBus.Write(constructedID, SendPacket.Payload);
+        }
+
+        private void ReadOnThread()
+        {
+            while (ReadEnable)
+            {
+                // Read
+                Tuple<uint, byte[]> CanRead = CANBus.Read();
+
+                byte priority = Convert.ToByte(((CanRead.Item1) >> 8) & 0x01);
+                byte sender = Convert.ToByte(((CanRead.Item1) >> 4) & 0x0F);
+                byte receiver = Convert.ToByte((CanRead.Item1) & 0x0F);
+
+                Packet newPack = new Packet
+                {
+                    TargetDeviceID = (Device)sender,
+                    Priority = priority == 0 ? false : true,
+                    PacketType = (CANPacket)CanRead.Item2[0],
+                    Payload = CanRead.Item2
+                };
+
+                lock (ReceiveQueue) { ReceiveQueue.Enqueue(newPack); }
+
+                Thread.Sleep(Constants.DEFAULT_MIN_THREAD_SLEEP);
+            }
+        }
+
+        private static byte[] ConstructCanID(Packet packet)
+        {
+            byte[] canID = new byte[3];
+            canID[2] = (byte)(packet.Priority ? 0x01 : 0x00);
+            canID[1] = DEVICE_ADDR;
+            canID[0] = (byte)packet.TargetDeviceID;
+            return canID;
+        }
+
+        public Packet? ReadNext()
+        {
+            return ReceiveQueue.Count == 0 ? (Packet?)null : ReceiveQueue.Dequeue();
+        }
+
+        public int ReceiveQueueSize() { return ReceiveQueue.Count; }
+
+    }
+}
---
+++ ArmInterface/ArmInterface/Enums.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmInterface
+{
+    public enum CANPacket : byte
+    {
+        MODE_SELECT = 0x00,
+        SPEED_DIR = 0x00,
+        ANGLE_MAX_SPD = 0x00,
+        INDEX = 0x00,
+        RESET = 0x00,
+        SET_P = 0x00,
+        SET_I = 0x00,
+        SET_D = 0x00,
+        SET_TICKS_PER_REV = 0x00,
+        MODEL_REQ = 0x00,
+        MODEL_NUM = 0x00,
+        ENCODER_CNT = 0x00,
+        STATUS = 0x00,
+        TELEMETRY = 0x00,
+        ERROR_MSG = 0x00
+    }
+
+    public enum Device : byte
+    {
+        BASE = 0x00,
+        SHOULDER = 0x01,
+        ELBOW = 0x02,
+        WRIST = 0x03,
+        DIFFERENTIAL_1 = 0x04,
+        DIFFERENTIAL_2 = 0x05,
+        HAND = 0x06
+    }
+
+}
